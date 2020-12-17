0e2bb3dd24fefedd16d04a47ee00d4ea02872574
shanedabes <shane@isda.best>
2020-12-17 20:05:27 +0000

    first working notification server

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/shanedabes/curst
 
 go 1.15
 
-require (
-	github.com/TheCreeper/go-notify v0.2.0
-	github.com/google/martian v2.1.0+incompatible
-)
+require github.com/TheCreeper/go-notify v0.2.0
--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 
-	notify "github.com/TheCreeper/go-notify"
+	"github.com/TheCreeper/go-notify"
 )
 
-func main() {
-	notification := notify.Notification{
-		AppName: "curst",
-		AppIcon: "/tmp/img.jpg",
-		Summary: "Test notification",
-		Body:    "Hello I am a notification",
-		Timeout: 5000,
-	}
+type NotifyRequest struct {
+	AppIcon string `json:"appicon,omitempty"`
+	Summary string `json:"summary"`
+	Body    string `json:"body"`
+	Timeout int32  `json:"timeout,omitempty"`
+}
 
-	if _, err := notification.Show(); err != nil {
-		log.Fatal("Notification failed")
+func main() {
+	http.HandleFunc("/notify", func(w http.ResponseWriter, r *http.Request) {
+		nr := NotifyRequest{}
+		err := json.NewDecoder(r.Body).Decode(&nr)
+		if err != nil {
+			log.Fatal("Notification request failed")
+		}
+
+		n := notify.Notification{
+			AppName: "curst",
+			AppIcon: "/tmp/img.jpg",
+			Summary: nr.Summary,
+			Body:    nr.Body,
+			Timeout: 5000,
+		}
+
+		if _, err := n.Show(); err != nil {
+			log.Print("Error showing notification")
+		}
+	})
+
+	err := http.ListenAndServe(":4590", nil)
+	if err != nil {
+		log.Fatal("Http listen failed")
 	}
 }
