904327ddddfcac1d71aa6a1c53e98c000c129b3d
shanedabes <shane@isda.best>
2020-12-17 23:43:02 +0000

    add command line configuration options

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/shanedabes/curst
 
 go 1.15
 
-require github.com/TheCreeper/go-notify v0.2.0
+require (
+	github.com/TheCreeper/go-notify v0.2.0
+	github.com/jessevdk/go-flags v1.4.0
+)
--- main.go
+++ main.go
@@ -2,25 +2,49 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/TheCreeper/go-notify"
+	notify "github.com/TheCreeper/go-notify"
+	"github.com/jessevdk/go-flags"
 )
+
+type Options struct {
+	Port    uint16 `short:"p" long:"port" description:"port to listen on" env:"CURST_PORT" default:"4950"`
+	Timeout int32  `short:"t" long:"timeout" description:"Default notification timeout" env:"CURST_TIMEOUT" default:"5000"`
+}
 
 type NotifyRequest struct {
 	AppIcon string `json:"appicon,omitempty"`
-	Summary string `json:"summary"`
-	Body    string `json:"body"`
+	Summary string `json:"summary,omitempty"`
+	Body    string `json:"body,omitempty"`
 	Timeout int32  `json:"timeout,omitempty"`
 }
 
+func defInt(x, y int32) int32 {
+	if x > 0 {
+		return x
+	}
+	return y
+}
+
 func main() {
+	opts := Options{}
+
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		os.Exit(1)
+	}
+
 	http.HandleFunc("/notify", func(w http.ResponseWriter, r *http.Request) {
 		nr := NotifyRequest{}
 		err := json.NewDecoder(r.Body).Decode(&nr)
 		if err != nil {
-			log.Fatal("Notification request failed")
+			log.Print("Notification request failed")
+
+			return
 		}
 
 		n := notify.Notification{
@@ -28,7 +52,7 @@ func main() {
 			AppIcon: "/tmp/img.jpg",
 			Summary: nr.Summary,
 			Body:    nr.Body,
-			Timeout: 5000,
+			Timeout: defInt(nr.Timeout, opts.Timeout),
 		}
 
 		if _, err := n.Show(); err != nil {
@@ -36,7 +60,7 @@ func main() {
 		}
 	})
 
-	err := http.ListenAndServe(":4590", nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", opts.Port), nil)
 	if err != nil {
 		log.Fatal("Http listen failed")
 	}
